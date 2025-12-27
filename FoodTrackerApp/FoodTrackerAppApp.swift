5566d55803777eb7a2cb176ec87212754f273b16
Requime31 <rs6063187@gmail.com>
2025-12-27 19:03:20 +0100

    Initial Commit

---
+++ FoodTrackerApp/FoodTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTrackerAppApp.swift
+//  FoodTrackerApp
+//
+//  Created by Roman Shevchenko on 27/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
